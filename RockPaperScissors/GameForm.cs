2064cac5943ae4e6c6cb3a68b93885224dff28a8
GaProgMan <jamie@taylorj.org.uk>
2014-02-08 18:54:30 +0000

    Moved game processing to windows form

---
+++ RockPaperScissors/GameForm.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RockPaperScissors
+{
+   public partial class GameForm : Form
+   {
+      private Round.moves userMove;
+      private Round.moves cpuMove;
+      private Round currentRound;
+      public List<Round> gameData;
+
+      public GameForm()
+      {
+         InitializeComponent();
+
+         // Set up drop down list with all values from the Round.moves
+         // enumeration, and default to the 0th item
+         drpAvailableMoves.Items.Clear();
+         foreach (string moveName in Enum.GetNames(typeof(Round.moves)))
+         {
+            drpAvailableMoves.Items.Add(moveName);
+         }
+         drpAvailableMoves.SelectedIndex = 0;
+
+         // Ensure that we have a new list to store all game data in
+         gameData = new List<Round>();
+      }
+
+      private void btnUseMove_Click(object sender, EventArgs e)
+      {
+         // Parse the user input to a Round.moves enum value
+         userMove = parseUserInput(drpAvailableMoves.SelectedItem.ToString());
+
+         // Generate the data for this round
+         cpuMove = generateGuess();
+         currentRound = new Round(userMove, cpuMove);
+
+         // Write the result of this round to the log
+         rtbResults.AppendText(currentRound.ToString());
+
+         // Add the data from this round to the list of previous
+         // rounds
+         gameData.Add(currentRound);
+      }
+
+      /// <summary>
+      /// Used to parse the user input move string to a Round.moves enum
+      /// </summary>
+      /// <param name="inUserInput">The user onput move string</param>
+      /// <returns>A Round.moves enum</returns>
+      private Round.moves parseUserInput(string inUserInput)
+      {
+         switch (inUserInput)
+         {
+            case "Rock":
+               return Round.moves.Rock;
+            case "Paper":
+               return Round.moves.Paper;
+            case "Scissors":
+               return Round.moves.Scissors;
+            case "Lizard":
+               return Round.moves.Lizard;
+            case "Spock":
+               return Round.moves.Spock;
+            default:
+               return Round.moves.q;
+         }
+      }
+
+      /// <summary>
+      /// Weak random generation of cpu guess, using the number
+      /// of seconds in the current minute as the seed for a
+      /// randomly generated number. This number is then used
+      /// as a basis for whether the cpu guess is Rock (1),
+      /// Paper (2) or Scissors (3)
+      /// </summary>
+      /// <returns>Cpu guess (Rock, Paper or Scissors)</returns>
+      private Round.moves generateGuess()
+      {
+         // Extremely bad algorithm for generating the CPU guess
+         Random randChoice = new Random(System.DateTime.Now.Second);
+         switch (randChoice.Next(1, 5))
+         {
+            case 1:
+               return Round.moves.Rock;
+            case 2:
+               return Round.moves.Paper;
+            case 3:
+               return Round.moves.Scissors;
+            case 4:
+               return Round.moves.Lizard;
+            case 5:
+               return Round.moves.Spock;
+            default:
+               throw new Exception("Random value generated was not as expected");
+         }
+      }
+   }
+}
--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -11,7 +11,7 @@ namespace RockPaperScissors
    /// </summary>
    public class Round
    {
-      public enum moves { rock, paper, scissors, lizard, spock, q };
+      public enum moves { Rock, Paper, Scissors, Lizard, Spock, q };
 
       private moves _userMove;
       private moves _cpuMove;
@@ -30,34 +30,34 @@ namespace RockPaperScissors
       {
          switch (_userMove)
          {
-            case moves.rock:
-               if (_cpuMove == moves.scissors || _cpuMove == moves.lizard)
+            case moves.Rock:
+               if (_cpuMove == moves.Scissors || _cpuMove == moves.Lizard)
                   return "user";
-               else if(_cpuMove == moves.spock || _cpuMove == moves.paper)
+               else if(_cpuMove == moves.Spock || _cpuMove == moves.Paper)
                   return "cpu";
                break;
-            case moves.paper:
-               if (_cpuMove == moves.rock || _cpuMove == moves.spock)
+            case moves.Paper:
+               if (_cpuMove == moves.Rock || _cpuMove == moves.Spock)
                   return "user";
-               else if(_cpuMove == moves.lizard || _cpuMove == moves.scissors)
+               else if(_cpuMove == moves.Lizard || _cpuMove == moves.Scissors)
                   return "cpu";
                break;
-            case moves.scissors:
-               if (_cpuMove == moves.paper || _cpuMove == moves.lizard)
+            case moves.Scissors:
+               if (_cpuMove == moves.Paper || _cpuMove == moves.Lizard)
                   return "user";
-               else if(_cpuMove == moves.spock || _cpuMove == moves.rock)
+               else if(_cpuMove == moves.Spock || _cpuMove == moves.Rock)
                   return "cpu";
                break;
-            case moves.lizard:
-               if (_cpuMove == moves.paper || _cpuMove == moves.spock)
+            case moves.Lizard:
+               if (_cpuMove == moves.Paper || _cpuMove == moves.Spock)
                   return "user";
-               else if(_cpuMove == moves.scissors || _cpuMove == moves.rock)
+               else if(_cpuMove == moves.Scissors || _cpuMove == moves.Rock)
                   return "cpu";
                break;;
-            case moves.spock:
-               if (_cpuMove == moves.scissors || _cpuMove == moves.rock)
+            case moves.Spock:
+               if (_cpuMove == moves.Scissors || _cpuMove == moves.Rock)
                   return "user";
-               else if(_cpuMove == moves.paper || _cpuMove == moves.lizard)
+               else if(_cpuMove == moves.Paper || _cpuMove == moves.Lizard)
                   return "cpu";
                break;
          }
@@ -83,107 +83,109 @@ namespace RockPaperScissors
       static void Main(string[] args)
       {
          // Create a list of rounds to store all game data in
-         List<Round> previousRounds = new List<Round>();
+         // List<Round> previousRounds = new List<Round>();
 
          // Primitive game loop - continue looping here until the
          // user/player wants to quit
-         while (true)
-         {
-            Console.WriteLine("Enter your choice (Rock, Paper, Scissors, Lizard or Spock - or \"q\" to quit): ");
+         //while (true)
+         //{
+            GameForm newGameForm = new GameForm();
+            newGameForm.ShowDialog();
+            //Console.WriteLine("Enter your choice (Rock, Paper, Scissors, Lizard or Spock - or \"q\" to quit): ");
 
-            // Parse the user input to a Round.moves enum value
-            Round.moves userMove = parseUserInput(Console.ReadLine().ToLower());
+            //// Parse the user input to a Round.moves enum value
+            //Round.moves userMove = parseUserInput(Console.ReadLine().ToLower());
 
-            //string userChoice = Console.ReadLine().ToLower();
+            ////string userChoice = Console.ReadLine().ToLower();
 
-            // As long as the user doesn't enter "q", we'll keep playing
-            if (userMove != Round.moves.q)
-            {
-               // Generate the data for this round
-               Round newRound = new Round(userMove, generateGuess());
-               //Round newRound = new Round(userChoice, generateGuess());
+            //// As long as the user doesn't enter "q", we'll keep playing
+            //if (userMove != Round.moves.q)
+            //{
+            //   // Generate the data for this round
+            //   Round newRound = new Round(userMove, generateGuess());
+            //   //Round newRound = new Round(userChoice, generateGuess());
 
-               // Write the result of this round to the console
-               Console.WriteLine(Environment.NewLine + newRound.getResult());
+            //   // Write the result of this round to the console
+            //   Console.WriteLine(Environment.NewLine + newRound.getResult());
 
-               // Add the data from this round to the list of previous
-               // rounds
-               previousRounds.Add(newRound);
-            }
-            else
-               break;
-         }
+            //   // Add the data from this round to the list of previous
+            //   // rounds
+            //   previousRounds.Add(newRound);
+            //}
+            //else
+            //   break;
+         //}
 
-         // When the user/player decides to quit, dump the results
-         // from each round to the console
-         Console.WriteLine(Environment.NewLine);
-         if (previousRounds.Count > 0)
-         {
-            foreach (Round prevRound in previousRounds)
-            {
-               Console.WriteLine(prevRound.ToString());
-            }
-         }
-         else
-         {
-            Console.WriteLine("No rounds stored.");
-         }
+         //// When the user/player decides to quit, dump the results
+         //// from each round to the console
+         //Console.WriteLine(Environment.NewLine);
+         //if (previousRounds.Count > 0)
+         //{
+         //   foreach (Round prevRound in previousRounds)
+         //   {
+         //      Console.WriteLine(prevRound.ToString());
+         //   }
+         //}
+         //else
+         //{
+         //   Console.WriteLine("No rounds stored.");
+         //}
 
          // Give the user/player a chance to read the console data
-         Console.ReadLine();
+         //Console.ReadLine();
       }
 
-      /// <summary>
-      /// Weak random generation of cpu guess, using the number
-      /// of seconds in the current minute as the seed for a
-      /// randomly generated number. This number is then used
-      /// as a basis for whether the cpu guess is rock (1),
-      /// paper (2) or scissors (3)
-      /// </summary>
-      /// <returns>Cpu guess (rock, paper or scissors)</returns>
-      static Round.moves generateGuess()
-      {
-         // Extremely bad algorithm for generating the CPU guess
-         Random randChoice = new Random(System.DateTime.Now.Second);
-         switch (randChoice.Next(1, 5))
-         {
-            case 1:
-               return Round.moves.rock;
-            case 2:
-               return Round.moves.paper;
-            case 3:
-               return Round.moves.scissors;
-            case 4:
-               return Round.moves.lizard;
-            case 5:
-               return Round.moves.spock;
-            default:
-               throw new Exception("Random value generated was not as expected");
-         }
-      }
+      ///// <summary>
+      ///// Weak random generation of cpu guess, using the number
+      ///// of seconds in the current minute as the seed for a
+      ///// randomly generated number. This number is then used
+      ///// as a basis for whether the cpu guess is Rock (1),
+      ///// Paper (2) or Scissors (3)
+      ///// </summary>
+      ///// <returns>Cpu guess (Rock, Paper or Scissors)</returns>
+      //static Round.moves generateGuess()
+      //{
+      //   // Extremely bad algorithm for generating the CPU guess
+      //   Random randChoice = new Random(System.DateTime.Now.Second);
+      //   switch (randChoice.Next(1, 5))
+      //   {
+      //      case 1:
+      //         return Round.moves.Rock;
+      //      case 2:
+      //         return Round.moves.Paper;
+      //      case 3:
+      //         return Round.moves.Scissors;
+      //      case 4:
+      //         return Round.moves.Lizard;
+      //      case 5:
+      //         return Round.moves.Spock;
+      //      default:
+      //         throw new Exception("Random value generated was not as expected");
+      //   }
+      //}
 
-      /// <summary>
-      /// Used to parse the user input move string to a Round.moves enum
-      /// </summary>
-      /// <param name="inUserInput">The user onput move string</param>
-      /// <returns>A Round.moves enum</returns>
-      static Round.moves parseUserInput(string inUserInput)
-      {
-         switch (inUserInput)
-         {
-            case "rock":
-               return Round.moves.rock;
-            case "paper":
-               return Round.moves.paper;
-            case "scissors":
-               return Round.moves.scissors;
-            case "lizard":
-               return Round.moves.lizard;
-            case "spock":
-               return Round.moves.spock;
-            default:
-               return Round.moves.q;
-         }
-      }
+      ///// <summary>
+      ///// Used to parse the user input move string to a Round.moves enum
+      ///// </summary>
+      ///// <param name="inUserInput">The user onput move string</param>
+      ///// <returns>A Round.moves enum</returns>
+      //static Round.moves parseUserInput(string inUserInput)
+      //{
+      //   switch (inUserInput)
+      //   {
+      //      case "Rock":
+      //         return Round.moves.Rock;
+      //      case "Paper":
+      //         return Round.moves.Paper;
+      //      case "Scissors":
+      //         return Round.moves.Scissors;
+      //      case "Lizard":
+      //         return Round.moves.Lizard;
+      //      case "Spock":
+      //         return Round.moves.Spock;
+      //      default:
+      //         return Round.moves.q;
+      //   }
+      //}
    }
 }
